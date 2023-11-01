fbb6fdf4d56fa6758558b62b9a5f74202287a3cf
kcccendana <kc_telyn@yahoo.com>
2023-11-01 15:52:23 +0900

    add test for user controller

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,17 +1,30 @@
 require 'rails_helper'
 
-RSpec.describe 'Users', type: :request do
-  describe 'GET /index' do
-    it 'returns http success' do
-      get '/users/index'
-      expect(response).to have_http_status(:success)
+RSpec.describe UsersController, type: :controller do
+  describe 'GET #index' do
+    it 'assigns @users' do
+      user = User.create(name: 'Ben', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Poland.')
+      get :index
+      expect(assigns(:users)).to eq([user])
+    end
+
+    it 'renders the index template' do
+      get :index
+      expect(response).to render_template('index')
     end
   end
 
-  describe 'GET /show' do
-    it 'returns http success' do
-      get '/users/show'
-      expect(response).to have_http_status(:success)
+  describe 'GET #show' do
+    it 'assigns @user' do
+      user = User.create(name: 'Kyla', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Poland.')
+      get :show, params: { id: user.id }
+      expect(assigns(:user)).to eq(user)
+    end
+
+    it 'renders the show template' do
+      user = User.create(name: 'Test3', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher3 from Poland.')
+      get :show, params: { id: user.id }
+      expect(response).to render_template('show')
     end
   end
 end
