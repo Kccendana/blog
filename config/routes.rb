84f14bacf544bd95bc8306041070389c96639718
kcccendana <kc_telyn@yahoo.com>
2023-11-03 16:46:22 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
+  # get 'comments/index'
+  # get 'comments/new'
+  # get 'comments/create'
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show]
+    resources :posts, only: [:index, :show, :new, :create] do
+      resources :comments, only: [:new, :create]
+      post 'like', on: :member
+      # post 'like', to: 'posts#like'
+    end
   end
-  # # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # # Can be used by load balancers and uptime monitors to verify that the app is live.
-  # get "up" => "rails/health#show", as: :rails_health_check
-
-  # # Defines the root path route ("/")
-  # # root "posts#index"
+  root 'users#index'
 end
