23f12b2b00b3ecc6e597e04e2901b9e2cbd2698c
kcccendana <kc_telyn@yahoo.com>
2023-11-03 18:18:36 +0900

    add

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,6 @@
 Rails.application.routes.draw do
-  # get 'comments/index'
-  # get 'comments/new'
-  # get 'comments/create'
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new, :create] do
-      resources :comments, only: [:new, :create]
-      post 'like', on: :member
-      # post 'like', to: 'posts#like'
     end
   end
   root 'users#index'
