142cb862623f9b5a37cd865c4db381dab87cb929
kcccendana <kc_telyn@yahoo.com>
2023-11-03 18:32:01 +0900

    modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new, :create] do
+      resources :comments, only: [:new, :create]
+      post 'like', on: :member
     end
   end
   root 'users#index'
